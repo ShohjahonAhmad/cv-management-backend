2a861528674bc433f76cfc43f76b46f78c3242c5
ShohjahonAhmad <shox_0502@mail.ru>
2026-07-26 16:00:39 +0200

    wromng route matching

--- src/routes/candidate.ts
+++ src/routes/candidate.ts
@@ -6,12 +6,12 @@ import * as validation from "../middleware/validation.js";
 const router = Router();
 
 router.get("/profile", candidateController.getProfile);
+router.post("/profile/attributes", validation.AddAttribute, candidateController.addAttributes);
+router.get("/profile/attributes", candidateController.searchAttributes);
 router.put("/profile/:id", validation.validateId, candidateController.updateProfile);
 router.get("/profile/:id", validation.validateId, candidateController.getProfileById);
 router.post("/profile/avatar", upload.single("avatar"), candidateController.uploadAvatar);
 router.post("/profile/image/:attributeValueId", upload.single("image"), candidateController.uploadImageAttribute);
-router.post("/profile/attributes", validation.AddAttribute, candidateController.addAttributes);
-router.get("/profile/attributes", candidateController.searchAttributes);
 router.get("/positions", candidateController.getPositions);
 router.get("/positions/:id", candidateController.getPositionById);
 
